274a71f346c81c59ffa73e1ed4f4f9cc2a91a75a
EshanDeSilva <hasindueshandesilva@gmail.com>
2023-05-07 19:56:24 +0530

    3

--- src/controller/CalFormController.java
+++ src/controller/CalFormController.java
@@ -74,24 +74,123 @@ public class CalFormController {
     private TextField txtShow;
 
     private List<Button> btnList;
+    private double answer;
+    private String function;
 
     public void initialize(){
+        answer=0;
+        function="empty";
         btnList = FXCollections.observableArrayList(btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btnPoint,btnEq,
                 btnSum,btnSubs,btnMulti,btnDev,btnC,btnCE);
         off();
         onOffFunction();
+        setAnswer();
+
+        functions();
+    }
+
+    private void functions() {
+        btnC.setOnAction(event -> {
+            txtShow.clear();
+        });
+        btnCE.setOnAction(event -> {
+            function = "empty";
+            answer = 0;
+            lblAnswer.setText("0");
+        });
+
+        btn0.setOnAction(event -> {
+            txtShow.setText(txtShow.getText()!=null? txtShow.getText()+"0":"0");
+        });
+        btn1.setOnAction(event -> {
+            txtShow.setText(txtShow.getText()!=null? txtShow.getText()+"1":"1");
+        });
+        btn2.setOnAction(event -> {
+            txtShow.setText(txtShow.getText()!=null? txtShow.getText()+"2":"2");
+        });
+        btn3.setOnAction(event -> {
+            txtShow.setText(txtShow.getText()!=null? txtShow.getText()+"3":"3");
+        });
+        btn4.setOnAction(event -> {
+            txtShow.setText(txtShow.getText()!=null? txtShow.getText()+"4":"4");
+        });
+        btn5.setOnAction(event -> {
+            txtShow.setText(txtShow.getText()!=null? txtShow.getText()+"5":"5");
+        });
+        btn6.setOnAction(event -> {
+            txtShow.setText(txtShow.getText()!=null? txtShow.getText()+"6":"6");
+        });
+        btn7.setOnAction(event -> {
+            txtShow.setText(txtShow.getText()!=null? txtShow.getText()+"7":"7");
+        });
+        btn8.setOnAction(event -> {
+            txtShow.setText(txtShow.getText()!=null? txtShow.getText()+"8":"8");
+        });
+        btn9.setOnAction(event -> {
+            txtShow.setText(txtShow.getText()!=null? txtShow.getText()+"9":"9");
+        });
+        btnPoint.setOnAction(event -> {
+            txtShow.setText(txtShow.getText()!=null? txtShow.getText()+".":"0.");
+        });
+        btnSum.setOnAction(event -> {
+            action();
+            function = "sum";
+        });
+        btnSubs.setOnAction(event -> {
+            action();
+            function = "sub";
+        });
+        btnMulti.setOnAction(event -> {
+            action();
+            function = "multi";
+        });
+        btnDev.setOnAction(event -> {
+            action();
+            function = "dev";
+        });
+        btnEq.setOnAction(event -> {
+            action();
+            function = "equal";
+        });
+    }
+
+    private void setAnswer(){
+        lblAnswer.setText(String.format("%.02f",answer));
+        txtShow.clear();
+    }
+
+    private void action() {
+        switch (function){
+            case "empty" : answer = Double.parseDouble(txtShow.getText());
+                            setAnswer();break;
+            case "sum" : answer += Double.parseDouble(txtShow.getText());
+                            setAnswer();break;
+            case "sub" : answer -= Double.parseDouble(txtShow.getText());
+                            setAnswer();break;
+            case "multi" : answer *= Double.parseDouble(txtShow.getText());
+                            setAnswer();break;
+            case "dev" : answer /= Double.parseDouble(txtShow.getText());
+                            setAnswer();break;
+            case "equal" : setAnswer();break;
+        }
     }
 
     private void off() {
         for (Button btn:btnList) {
             btn.setDisable(true);
         }
+        txtShow.clear();
+        answer = 0;
+        setAnswer();
     }
 
     private void on() {
         for (Button btn:btnList) {
             btn.setDisable(false);
         }
+        txtShow.clear();
+        answer = 0;
+        setAnswer();
     }
 
     private void onOffFunction() {
